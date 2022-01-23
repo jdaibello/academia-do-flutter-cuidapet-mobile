89aa28488c497f08e5dbb0bea17e4ef5d7cc7092
João Pedro <jpedrodaibello1999@outlook.com>
2022-01-23 12:13:29 -0300

    feat: add updating packages and code hits

--- lib/app/core/ui/ui_config.dart
+++ lib/app/core/ui/ui_config.dart
@@ -9,6 +9,5 @@ class UiConfig {
         primaryColor: const Color(0xFFA8CE48),
         primaryColorDark: const Color(0xFF689F38),
         primaryColorLight: const Color(0xFFDDEDC7),
-        accentColor: const Color(0xFF4CAF50),
       );
 }
